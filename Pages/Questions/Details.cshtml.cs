603a5d945618358e3fc3c854e0ee63e11d74cf29
Douglas Starnes <douglas.a.starnes@gmail.com>
2020-04-29 15:42:40 -0500

    Pushed from Linux

---
+++ Pages/Questions/Details.cshtml.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Quanda.Models;
+
+namespace Quanda.Pages.Questions
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly QuandaDbContext context;
+        public Question Question { get; set; }
+
+        public DetailsModel(QuandaDbContext context)
+        {
+            this.context = context;
+        }
+        public void OnGet(int questionId)
+        {
+            Question = context.Questions.FirstOrDefault(q => q.QuestionId == questionId);
+        }
+    }
+}
